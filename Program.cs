3372bb3479a6b32dbc026a8175c37be627f39e2e
Garccosta <garccosta@gmail.com>
2021-03-10 20:31:32 -0300

    Created basic dotnet console structure fro the app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_Bank_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
